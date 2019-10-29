233048b933b8498aab8b5ff8c424618560e68e38
Wolaci <wolacijose8@gmail.com>
2019-10-29 08:07:32 -0300

    Excluindo arquivo desnecessário

--- resources/views/checkout/checkoutPatientView.blade.php
+++ resources/views/checkout/checkoutPatientView.blade.php
@@ -3,10 +3,7 @@
 @section('content')
     <div class="container justify-content-center">
         <div class="row">
-            <h1>Boa sorte!, Façam a mágica agora :P, eu passo o poder para vocês S2 </h1>
-            <div style="margin:auto;">
-                <img src="{{asset('images/download.gif')}}" alt="">
-            </div>
+
         </div>
     </div>
 @endsection
